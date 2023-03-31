2de9e45ed5decba47c8da1d427284fd667c50183
i-10101000 <ilya.krasnoper@gmail.com>
2023-03-31 22:48:14 +0300

    ref-1.1

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import logo from './img/logo.png'
+import {MdOutlineKeyboardArrowRight, MdProductionQuantityLimits} from 'react-icons/md'
+import {FiHome} from 'react-icons/fi'
+import {GoSearch} from 'react-icons/go'
+import {BiCategoryAlt} from 'react-icons/bi'
+import {FaUsers} from 'react-icons/fa'
 
 export default function Sidebar(){
 
@@ -15,7 +20,42 @@ export default function Sidebar(){
                         <span className="profession">React Redux Less</span>
                     </div>
                 </div>
+                <span className="toogle"><MdOutlineKeyboardArrowRight/></span>
             </header>
+            <div className="menu-bar">
+                <div className="menu">
+                    <li className="search-box">
+                            <span className="icon"><GoSearch/></span>
+                            <input type="search" placeholder="Search..." />
+                    </li>
+                    <ul className="menu-links">
+                        <li className="nav-link">
+                            <a href="#">
+                                <span className="icon"><FiHome/></span>
+                                <span className="text nav-text">Home</span>
+                            </a>
+                        </li>
+                        <li className="nav-link">
+                            <a href="#">
+                                <span className="icon"><MdProductionQuantityLimits/></span>
+                                <span className="text nav-text">Products</span>
+                            </a>
+                        </li>
+                        <li className="nav-link">
+                            <a href="#">
+                                <span className="icon"><BiCategoryAlt/></span>
+                                <span className="text nav-text">Category</span>
+                            </a>
+                        </li>
+                        <li className="nav-link">
+                            <a href="#">
+                                <span className="icon"><FaUsers/></span>
+                                <span className="text nav-text">Users</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
